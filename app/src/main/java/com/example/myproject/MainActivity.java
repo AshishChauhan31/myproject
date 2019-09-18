5f3cdd6d519792b737ccd9fe15688b20bb3433d4
ashish <ashish.chauhan_mca18@gla.ac.in>
2019-09-18 20:13:58 +0530

    Init commit

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
