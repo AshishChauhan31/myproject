88df6d4b4a6353bbf3ee228a01a8c1cca68e7a17
ashish <ashish.chauhan_mca18@gla.ac.in>
2019-09-18 23:24:28 +0530

    BASIC LOGIN ACTIVITY CREATED WITH TWO TEXTVIEW,EMAIL,PASSWORD FIELDS AND SUBMIT BUTTON.

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -6,12 +6,40 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView emailview,passview;
+    EditText Email,Pass;
+
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        emailview = findViewById(R.id.emailview);
+        passview = findViewById(R.id.passview);
+
+        Email = findViewById(R.id.Email);
+        Pass = findViewById(R.id.Pass);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"love u ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
